61e6b6600d0c813c1d2b2c1868127efde6c4e149
Nikolay <Horushko.Nikolay@gmail.com>
2016-04-12 20:54:33 +0300

    Initial commit

---
+++ src/my/project/juja/DataBaseManager.java
@@ -0,0 +1,11 @@
+package my.project.juja;
+
+/**
+ * Created by Nikol on 4/12/2016.
+ */
+public class DataBaseManager {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("please, input your command or 'help'");
+    }
+}
