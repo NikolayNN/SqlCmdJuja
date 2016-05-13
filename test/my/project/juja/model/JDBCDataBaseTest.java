0c9be3b83dd10c4ab18311ac1a7217f0e67262e7
Nikolay <Horushko.Nikolay@gmail.com>
2016-05-13 20:51:01 +0300

    создана отдельная точка входа в программу

---
+++ test/my/project/juja/model/JDBCDataBaseTest.java
@@ -0,0 +1,72 @@
+package my.project.juja.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Nikol on 4/15/2016.
+ */
+public class JDBCDataBaseTest {
+    JDBCDataBase JDBCDataBase;
+    @Before
+    public void setup() {
+        JDBCDataBase = new JDBCDataBase();
+        JDBCDataBase.getConnection("sqlcmd", "postgres", "root");
+    }
+    @Test
+    public void testAddRecord(){
+
+        JDBCDataBase.clearTable("users");
+
+        JDBCDataBase.addRecord("users", "alex", "FadH74Gne", "123");
+
+        ArrayList<String> recordList = JDBCDataBase.getTableData("users");
+        String actual = recordList.get(0);
+
+        assertEquals("alex|FadH74Gne|123|", actual);
+
+    }
+    @Test
+    public void getColumnNameTest(){
+        String actual = JDBCDataBase.getColumnName("users");
+        assertEquals("name|password|Id|", actual);
+    }
+
+    @Test
+    public void getTableListTest(){
+        String actual = JDBCDataBase.getTableList();
+        assertEquals("[users2, users]", actual);
+    }
+
+    @Test
+    public void getTableData(){
+        JDBCDataBase.clearTable("users");
+        JDBCDataBase.addRecord("users", "login1", "pass1", "1");
+        JDBCDataBase.addRecord("users", "login2", "pass2", "2");
+        JDBCDataBase.addRecord("users", "login3", "pass3", "3");
+
+        List<String> actualList = JDBCDataBase.getTableData("users");
+        String actual = "";
+        for (String s : actualList) {
+            actual += s + "\n";
+        }
+
+        assertEquals(   "login1|pass1|1|\n" +
+                        "login2|pass2|2|\n" +
+                        "login3|pass3|3|\n", actual);
+    }
+
+
+
+
+
+
+
+
+}
